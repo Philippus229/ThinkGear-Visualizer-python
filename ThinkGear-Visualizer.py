fdb00ba93c6ccbb96a4b8e608e2f47ad1af49b3d
Philipp Geßner <philippus229@gmail.com>
2020-03-20 04:32:24 +0100

    Add files via upload

--- ThinkGear-Visualizer.py
+++ ThinkGear-Visualizer.py
@@ -1,8 +1,8 @@
 import serial, math, pygame
 
-codes = [0x02,     0x03,       0x04,       0x05,        0x06,      0x80,     0x83 ]
-names = ["quality","heartrate","attention","meditation","8bit_raw","eeg_raw","eeg"]
-c_len = [1,        1,          1,          1,           1,         3,        25   ]
+codes = [0x02,     0x03,       0x04,       0x05,        0x06,      0x80,     0x83      ]
+names = ["quality","heartrate","attention","meditation","8bit_raw","eeg_raw","eeg_asic"]
+c_len = [1,        1,          1,          1,           1,         3,        25        ]
 
 bands = ["delta","theta","low-alpha","high-alpha","low-beta","high-beta","low-gamma","mid-gamma"]
 
@@ -14,10 +14,10 @@ def signed_thing_to_int(b, length):
 class ThinkGear(object):
     def __init__(self, port, baudrate=57600):
         self.ser = serial.Serial(port, baudrate) #initialize serial communication/connection
-        self.values = {}
+        self.data = {}
 
     def fetch_data(self):
-        self.values = {} #reset values
+        self.data = {} #reset values
         while True:
             self.ser.read_until(b"\xAA\xAA") #wait for sync bytes
             plength = ord(self.ser.read(1)) #payload length
@@ -31,16 +31,16 @@ class ThinkGear(object):
             if code in codes: #check if current byte is a supported code
                 c = codes.index(code) #find corresponding index in the three code-related lists above
                 '''old code which I prefer (because it's technically one line) (sadly without a way to add comments)
-                self.values[names[c]] = payload[i+1] if c < 5 \
+                self.data[names[c]] = payload[i+1] if c < 5 \
                                         else signed_thing_to_int(payload[i+2] << 8 | payload[i+3], 16) if c == 5 \
                                         else dict(zip(bands, [payload[b] << 16 | payload[b+1] << 8 | payload[b+2] for b in range(i+1, i+25, 3)]))
                 '''
                 if c < 5: #all single-byte codes (quality, heartrate, attention, meditation, 8bit_raw)
-                    self.values[names[c]] = payload[i+1]
+                    self.data[names[c]] = payload[i+1]
                 elif c == 5: #eeg_raw (fun fact: the first byte after the code is completely useless)
-                    self.values[names[c]] = signed_thing_to_int(payload[i+2] << 8 | payload[i+3], 16)
-                elif c == 6: #eeg
-                    self.values[names[c]] = dict(zip(bands, [payload[b] << 16 | payload[b+1] << 8 | payload[b+2] for b in range(i+1, i+25, 3)]))
+                    self.data[names[c]] = signed_thing_to_int(payload[i+2] << 8 | payload[i+3], 16)
+                elif c == 6: #eeg_asic
+                    self.data[names[c]] = dict(zip(bands, [payload[b] << 16 | payload[b+1] << 8 | payload[b+2] for b in range(i+1, i+25, 3)]))
                 i += c_len[c] #add code-specific number of bytes to i
             i += 1 #add 1 each time to avoid getting stuck on unused bytes
 
@@ -59,7 +59,7 @@ while True:
     try:
         #clock.tick(30)
         eeg_device.fetch_data()
-        if len(eeg_device.values) == 1: y_vals = y_vals[1:]+[int(size[1]/2-size[1]*eeg_device.values["eeg_raw"]/raw_eeg_range)]
+        if len(eeg_device.data) == 1: y_vals = y_vals[1:]+[int(size[1]/2-size[1]*eeg_device.data["eeg_raw"]/raw_eeg_range)]
         surface.fill((0,0,0)) #Do I really need to explain this?
         points = list(zip(x_vals, y_vals)) #zip x and y values to pairs (list(zip([x0,x1,...xN], [y0,y1,...,yN])) = [(x0,y0),(x1,y1),...,(xN,yN)])
         pygame.draw.lines(surface, (255,255,255), False, points) #draw continuous line segments through points
